13b6d8fb62623a1c2d97d584bcb4c434f97fa037
Syed Ali Hassan Raza <ali.hassan@logicielservice.com>
2026-05-26 07:30:23 -0400

    session-end [2026-05-26 07:30]

---
+++ target/debug/deps/futures_io-8ae66fe401db9558.d
@@ -0,0 +1,7 @@
+D:\Projects\DiscordToGlobalRelay\target\debug\deps\futures_io-8ae66fe401db9558.d: C:\Users\SyedAliHassanRaza\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-io-0.3.32\src\lib.rs
+
+D:\Projects\DiscordToGlobalRelay\target\debug\deps\libfutures_io-8ae66fe401db9558.rlib: C:\Users\SyedAliHassanRaza\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-io-0.3.32\src\lib.rs
+
+D:\Projects\DiscordToGlobalRelay\target\debug\deps\libfutures_io-8ae66fe401db9558.rmeta: C:\Users\SyedAliHassanRaza\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-io-0.3.32\src\lib.rs
+
+C:\Users\SyedAliHassanRaza\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\futures-io-0.3.32\src\lib.rs:
